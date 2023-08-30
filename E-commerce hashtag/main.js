54f5e0230f433c1cdbb20d51ffa470278107178f
Murilo Garcia da Costa <murilogccontato@gmail.com>
2023-08-30 00:04:45 -0300

    Conclusão de segunda Aula

--- E-commerce hashtag/main.js
+++ E-commerce hashtag/main.js
@@ -1,3 +1,6 @@
+import { inicializarCarrinho } from "./src/menuCarrinho";
+
+
 const catalogo = [
     {id: 1,
     nome: 'Camisa Larga com Bolso',
@@ -15,56 +18,63 @@ const catalogo = [
 },
     {id:3,
     nome:'Jaqueta com Efeito Camurça',
+    marca: 'Zara',
     preco:60,
     imagem: 'product-3.jpg',
     feminimo:false,
 },
     {id:4,
     nome:'Sobretudo com Mescla de Lã',
+    marca: 'Zara',
     preco:160,
     imagem: 'product-4.jpg',
     feminimo:false,
 },
     {id:5,
     nome:'Camisa Larga Acolchoada com Veludo Cotelê',
+    marca: 'Zara',
     preco:110,
     imagem: 'product-5.jpg',
     feminimo:false,
 },
     {id:6,
     nome:'Casaco de Lã com Botões',
+    marca: 'Zara',
     preco:170,
     imagem: 'product-6.jpg',
     feminimo:true,
 },
     {id:7,
     nome:'Casaco com botões',
+    marca: 'Zara',
     preco:75,
     imagem: 'product-7.jpg',
     feminimo:true,
 },
     {id:8,
     nome:'Colete Comprido com Cinto',
+    marca: 'Zara',
     preco:88,
     imagem: 'product-8.jpg',
     feminimo:true,
-}
+},
+
 ]
 
 for (const produtoCatalogo of catalogo){
  const cartaoProduto = 
-`<div id="card-produto-1">
+`<div class='border-solid border-2 border-sky-500 w-48 m-3' id="card-produto-${produtoCatalogo.id}">
     <img src="./assets/img/${produtoCatalogo.imagem}" alt="" style="height: 300px;">
     
-    <p>${produtoCatalogo.marca}</p>
-    <p>${produtoCatalogo.nome}</p>
-    <p>R$${produtoCatalogo.preco}</p>
-    <button>Adicionar</button>
+    <p class='marca'>${produtoCatalogo.marca}</p>
+    <p class='nomeProduto'>${produtoCatalogo.nome}</p>
+    <p class='preco'>R$${produtoCatalogo.preco}</p>
+    <button class='adcBotao'>Adicionar</button>
 </div>`;
 
 document.getElementById('conteiner-produto').innerHTML += cartaoProduto
 }
 
-
+inicializarCarrinho()
 
 
