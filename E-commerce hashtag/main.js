d70bfd45ac2147ad84342f339fd6cbc8987ab8d1
Murilo Garcia da Costa <murilogccontato@gmail.com>
2023-09-06 14:37:37 -0300

    Cuidando da incrementação de decrementação de produtos

--- E-commerce hashtag/main.js
+++ E-commerce hashtag/main.js
@@ -1,54 +1,54 @@
 const catalogo = [
-    {id: 1,
+    {id: '1',
     nome: 'Camisa Larga com Bolso',
     marca: 'Zara',
     preco: 70,
     imagem: 'product-1.jpg',
     feminimo:false,
 },
-    {id: 2,
+    {id: '2',
     nome: 'Casaco Reto com Lã',
     marca: 'Zara',
     preco: 85,
     imagem: 'product-2.jpg',
     feminimo:true,
 },
-    {id:3,
+    {id:'3',
     nome:'Jaqueta com Efeito Camurça',
     marca: 'Zara',
     preco:60,
     imagem: 'product-3.jpg',
     feminimo:false,
 },
-    {id:4,
+    {id:'4',
     nome:'Sobretudo com Mescla de Lã',
     marca: 'Zara',
     preco:160,
     imagem: 'product-4.jpg',
     feminimo:false,
 },
-    {id:5,
+    {id:'5',
     nome:'Camisa Larga Acolchoada com Veludo Cotelê',
     marca: 'Zara',
     preco:110,
     imagem: 'product-5.jpg',
     feminimo:false,
 },
-    {id:6,
+    {id:'6',
     nome:'Casaco de Lã com Botões',
     marca: 'Zara',
     preco:170,
     imagem: 'product-6.jpg',
     feminimo:true,
 },
-    {id:7,
+    {id:'7',
     nome:'Casaco com botões',
     marca: 'Zara',
     preco:75,
     imagem: 'product-7.jpg',
     feminimo:true,
 },
-    {id:8,
+    {id:'8',
     nome:'Colete Comprido com Cinto',
     marca: 'Zara',
     preco:88,
@@ -75,8 +75,26 @@ function inicializarCarrinho(){
     botaoFecharCarrinho.addEventListener('click',fecharCarrinho)
     botaoAbrirCarrinho.addEventListener('click',abrirCarrinho)
   }
-  
+
+const idsProdutoCarrinhoComQuantidade = {};
+ 
+function incrementarQuantidadeProduto(idProduto){
+    idsProdutoCarrinhoComQuantidade[idProduto]++;
+}
+
+function decrementarQuantidadeProduto(idProduto){
+    idsProdutoCarrinhoComQuantidade[idProduto]--;
+}
+
+
 function adicionarAoCarrinho(idProduto){
+    if (idProduto in idsProdutoCarrinhoComQuantidade){
+        incrementarQuantidadeProduto(idProduto);
+        return;
+        
+    }else{
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+
     const produto = catalogo.find(p => p.id===idProduto)
 
     const conteinerProdutosCarrinho = document.getElementById(`produtos-carrinho`)
@@ -93,10 +111,16 @@ function adicionarAoCarrinho(idProduto){
       <p class="tamanho-produto-carrinho">Tamanho: M</p>
       <p class="preco-produto-carrinho">$${produto.preco}</p>
     </div>
+    <div class='controle-quantidade'> 
+        <button id = 'menos-item'><i class="fa-solid fa-minus"></i></button>
+        <p id = 'quantidade-${produto.id}'>${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+        <button id = 'mais-item'><i class="fa-solid fa-plus"></i></button>
+    </div>
   
   </article>`
   conteinerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho
-  }
+    }
+}
 
 
 
